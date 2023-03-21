e396cff0c97f97f889f46e46a6ebcf1110419ad3
Loïc Dornel <creeperstone72.redstoner@gmail.com>
2023-03-21 17:22:55 +0100

    feat(back) : Spring Boot setup + Cocktail object

---
+++ src/main/java/com/creeperstone72/bartenders_companion_back/BackEndApplication.java
@@ -0,0 +1,13 @@
+package com.creeperstone72.bartenders_companion_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndApplication.class, args);
+    }
+
+}
