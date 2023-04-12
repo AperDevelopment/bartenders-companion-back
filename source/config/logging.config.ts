a9535f2a1ad70f15467ef75f0c7389f6a67d40f3
Loïc Dornel <creeperstone72.redstoner@gmail.com>
2023-04-12 23:12:12 +0200

    refactor(middleware): Extracted functions
    Split server into three files :
    - router.config.ts : Sets up the router
    - app.ts : Defines all the routes from root
    - server.ts : Creates and starts the server
    
    TODO: Find a library to be able to switch between console and file logging

--- source/config/logging.config.ts
+++ source/config/logging.config.ts
@@ -1,42 +1,28 @@
-const getTimeStamp = (): string => {
-    return new Date().toISOString();
-};
+const consoleLog = process.env.CONSOLE_LOG || true;
+
+const getTimeStamp = (): string => new Date().toISOString();
+
+const getMessage = (level: string, namespace: string, message: string, object?: any): string => `[${getTimeStamp()}] [${level}] [${namespace}] ${message}` + object ?? '';
+
+const getInfoMessage = (namespace: string, message: string, object?: any): string => getMessage('INFO', namespace, message, object);
+const getWarnMessage = (namespace: string, message: string, object?: any): string => getMessage('WARN', namespace, message, object);
+const getErrorMessage = (namespace: string, message: string, object?: any): string => getMessage('ERROR', namespace, message, object);
+const getDebugMessage = (namespace: string, message: string, object?: any): string => getMessage('DEBUG', namespace, message, object);
 
 const info = (namespace: string, message: string, object?: any) => {
-    if (object) {
-        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object);
-    } else {
-        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
-    }
+    console.info(getInfoMessage(namespace, message, object));
 };
 
 const warn = (namespace: string, message: string, object?: any) => {
-    if (object) {
-        console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`, object);
-    } else {
-        console.warn(`[${getTimeStamp()}] [WARN] [${namespace}] ${message}`);
-    }
+    console.warn(getWarnMessage(namespace, message, object));
 };
 
 const error = (namespace: string, message: string, object?: any) => {
-    if (object) {
-        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, object);
-    } else {
-        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
-    }
+    console.error(getErrorMessage(namespace, message, object));
 };
 
 const debug = (namespace: string, message: string, object?: any) => {
-    if (object) {
-        console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`, object);
-    } else {
-        console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
-    }
+    console.debug(getDebugMessage(namespace, message, object));
 };
 
-export default {
-    info,
-    warn,
-    error,
-    debug
-};
+export default { info, warn, error, debug };
